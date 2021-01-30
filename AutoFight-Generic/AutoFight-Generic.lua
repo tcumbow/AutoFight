b7b700ac4a3393929666cb6d418c02b3f7455cc0
Tom Cumbow <tcumbow@icloud.com>
2021-01-30 16:35:34 -0500

    function TargetShouldBeTaunted

--- AutoFight-Generic/AutoFight-Generic.lua
+++ AutoFight-Generic/AutoFight-Generic.lua
@@ -25,6 +25,24 @@ local function UnitHasRegen(unitTag)
 	return false
 end
 
+local function TargetShouldBeTaunted()
+	if (not DoesUnitExist('reticleover') or IsUnitDead('reticleover') or GetUnitType('reticleover') == 1 or GetUnitReaction('reticleover') ~= UNIT_REACTION_HOSTILE or GetUnitDifficulty("reticleover") < 3) then
+		local numAuras = GetNumBuffs('reticleover')
+		if (numAuras > 0) then
+			for i = 1, numAuras do
+				local name, _, _, _, _, _, _, _, _, _, _, _ = GetUnitBuffInfo('reticleover', i)
+				if name=="Taunt" then
+					return false
+				end
+			end
+		end
+		return true
+	else
+		return false
+	end
+
+end
+
 local LowestGroupHealthPercentWithoutRegen
 local LowestGroupHealthPercentWithRegen
 local LowestGroupHealthPercent
