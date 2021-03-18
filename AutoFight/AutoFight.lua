240d2b95c13c0df700a5271042c279cadf4aeea3
Tom Cumbow <tcumbow@icloud.com>
2021-03-17 21:16:40 -0400

    rough draft rotation

--- AutoFight/AutoFight.lua
+++ AutoFight/AutoFight.lua
@@ -197,7 +197,14 @@ local CharacterFirstName = "Hadara"
 
 local function AutoFightMain()
 	if AutoFightShouldNotAct() then DoNothing()
+	elseif Magicka()<15 and not Blocking() and not HeavyAttackInProgress() then HeavyAttack()
+	elseif not IHave("Summon Twilight Matriarch") then UseAbility(4)
+	elseif LowestGroupHealthPercent()<40 then UseAbility(4)
+	elseif not IHave("Summon Volatile Familiar") then WeaveAbility(2)
+	elseif VolatilePulseReady() then WeaveAbility(2)
+	elseif not IHave("Major Sorcery") then WeaveAbility(3)
 	elseif UltimateReady() and TargetIsHostileNpc() then UseUltimate()
+	elseif TargetIsHostileNpc() then WeaveAbility(1)
 	else DoNothing()
 	end
 end
